bcf4627e2c7fff37149dac1a877598f533f542c4
linkiz <slava.modin4@mail.ru>
2021-03-01 13:27:51 +0300

    LabOneThe

--- index.php
+++ index.php
@@ -27,11 +27,23 @@ $user_name = 'linkiz'; // укажите здесь ваше имя
         <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
 
         <nav class="user-menu">
-    <?php if($is_auth)?>
+    <?php if($is_auth):?>
         <div class ="user-menu_image">
-            <img src
+            <img src="img/user.jpg" width="40" height="40" alt="Пользователь">
         </div>
-    <?php end;?>
+            <div class="user-menu__logged">
+                <p><?php echo($user_name)?></p>
+            </div>
+            <?php else:?>
+            <ul class="user-menu__list">
+                <li class="user-menu__item">
+                    <a href="#">Регистрация</a>
+                </li>
+                <li class="user-menu__item">
+                    <a href="#">Вход</a>
+                </li>
+            </ul>
+    <?php endif;?>
         </nav>
     </div>
 </header>
