f5f2aa7dd11a563c98895a3819d9365cd552c851
linkiz <slava.modin4@mail.ru>
2021-03-01 13:20:01 +0300

    first commit

--- index.php
+++ index.php
@@ -27,9 +27,11 @@ $user_name = 'linkiz'; // укажите здесь ваше имя
         <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
 
         <nav class="user-menu">
-
-        <!-- здесь должен быть PHP код для показа меню и данных пользователя -->
-
+    <?php if($is_auth)?>
+        <div class ="user-menu_image">
+            <img src
+        </div>
+    <?php end;?>
         </nav>
     </div>
 </header>
