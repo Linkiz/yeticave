fa71b7983153b943758f8e48db9bc421744e6849
linkiz <slava.modin4@mail.ru>
2021-03-24 11:05:12 +0300

    lab10(1)

---
+++ adds.php
@@ -0,0 +1,82 @@
+
+<?php
+require_once 'fuction.php';
+require 'link.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $required_fields = ['lot-name', 'category', 'message', 'lot-rate', 'lot-step', 'lot-date'];
+    $errors = [];
+
+    foreach ($required_fields as $field) {
+        if (empty($_POST[$field])) {
+            $errors[$field] = "form__item--invalid";
+            $form_error = 'form--invalid';
+        }
+        if ($field == 'lot-rate') {
+            if (!filter_var($_POST[$field], FILTER_VALIDATE_INT)) {
+                $errors[$field] = 'Начальная цена должна быть корректной';
+            }
+            if (intval($_POST[$field]) <= 0) {
+                $errors[$field] = 'Начальная цена должна быть корректной';
+            }
+        }
+        if ($field == 'lot-step') {
+            if (!filter_var($_POST[$field], FILTER_VALIDATE_INT)) {
+                $errors[$field] = 'Шаг ставки должен быть корректным';
+            }
+            if (intval($_POST[$field]) < 0) {
+                $errors[$field] = 'Начальная цена должна быть корректной';
+            }
+        }
+    }
+    if(count($errors) !== 0) {
+        $page_content = compile_tamplate('add.php',
+            ['errors' => $errors,
+                'categories_list' => $categories_list]);
+    } else {
+        $lot = [
+            "image" => file_url ? 'img/user.jpg' : '',
+            "name" => $_POST['lot-name'],
+            "start_price" => $_POST['lot-rate'],
+            "rate" => $_POST['lot-step'],
+            "timer" => $_POST['lot-date'],
+            "category" => $_POST['category'],
+            "description" => $_POST['message'],
+            "account_id" => $_SESSION['auth']['account_id']
+        ];
+        $page_content = compile_tamplate( 'add.php',
+            ['categories_list' => $categories_list,
+                'lot' => $currentLot,
+                'lotID' => $lotID,
+                'data_list' => $data_list,
+                'Timer' => $Timer]);
+    }
+
+
+
+}
+else {
+    $page_content = compile_tamplate('add.php',
+        ['categories_list' => $categories_list,
+            'data_list' => $data_list
+        ]);
+
+}
+
+
+
+
+
+$layout_content = compile_tamplate('layout.php',
+    ['page_layout' => 'Главная страница',
+        'is_auth' => $is_auth,
+        'user_avatar' => $user_avatar,
+        'user_name' => $user_name,
+        'page_content' => $page_content,
+        'categories_list' => $categories_list
+    ]);
+
+print($layout_content);
+
+?>
